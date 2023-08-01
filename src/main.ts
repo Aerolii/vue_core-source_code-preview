c796b13bc9da3a993697fcf185118471f35ed5e9
weenie89 <weenie89@Zhang.local>
2023-08-01 10:26:11 +0800

    refactor: 删除部分逻辑

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,3 @@
 import { createApp } from 'vue';
-// import App from './App';
-import App from './App.vue';
-// import Div from './utils/useExportDefault';
-
-// console.log(Div);
-// console.log(App);
+import App from './App';
 createApp(App).mount('#app');
-
-// import './core/useCollectionProxy';
